7f839cd3701905e52ae067c01f337547231e5010
Fabian Glanzer <fabian.glanzer99@gmx.at>
2026-08-26 09:34:44 +0200

    config: define development and production API endpoints

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  webhookUrl: 'https://code-a-cuisine.app.n8n.cloud/webhook/generate-recipe',
+  quotaStatusUrl: 'https://code-a-cuisine.app.n8n.cloud/webhook/quota-status',
+  databaseUrl: 'https://code-a-cuisine-edd75-default-rtdb.europe-west1.firebasedatabase.app/',
+};
